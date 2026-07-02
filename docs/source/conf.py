3b9fb3d6635bb4f4d490d865c7e9f755d853cbd7
Reyn <86341393+StahlReyn@users.noreply.github.com>
2026-07-02 16:37:37 +0700

    Changed placeholder title

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Omori Modding Guide'
+copyright = '2026, Various'
+author = 'Various, Omori Modding Community'
 
 release = '0.1'
 version = '0.1.0'
